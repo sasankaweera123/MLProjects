700d9bfd5a17d9417b338bb1ad5887d642e5fe43
Sasanka <sajewsasanka1@gmail.com>
2023-04-17 11:43:51 +0530

    Add Code Comments
    - In the Unit File Code Comments are Added

--- supervisedml/unit_test/test_advertising.py
+++ supervisedml/unit_test/test_advertising.py
@@ -1,15 +1,17 @@
 import unittest
 import Advertising
 
-
+# test Advertising module using unittest
 class TestAdvertising(unittest.TestCase):
 
+    # test get_data in Advertising module
     def test_get_data(self):
         df = Advertising.pd.read_csv('../data/Advertising.csv')
         x, y = Advertising.get_data(df)
         self.assertEqual(x.shape, (200, 3))
         self.assertEqual(y.shape, (200,))
 
+    # test get_stats in Advertising module
     def test_create_model(self):
         df = Advertising.pd.read_csv('../data/Advertising.csv')
         model = Advertising.create_model(df)
@@ -17,14 +19,16 @@ class TestAdvertising(unittest.TestCase):
         self.assertEqual(model.intercept_.shape, ())
         self.assertEqual(model.predict([[2300, 130, 0]]).shape, (1,))
 
+    # test plot_reg in Advertising module
     def test_plot_reg(self):
         df = Advertising.pd.read_csv('../data/Advertising.csv')
         Advertising.test_plot_reg(df)
-    
+
+    # test plot_bar in Advertising module
     def test_plot_bar(self):
         df = Advertising.pd.read_csv('../data/Advertising.csv')
         Advertising.test_plot_bar(df)
 
-
+# run the test
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
--- supervisedml/unit_test/test_data_lin.py
+++ supervisedml/unit_test/test_data_lin.py
@@ -2,14 +2,17 @@ import unittest
 import data_lin
 
 
+# test data_lin module using unittest
 class TestDataLin(unittest.TestCase):
 
+    # test get_data in data_lin module
     def test_get_data(self):
         df = data_lin.pd.read_csv('../data/data_lin.csv')
         x, y = data_lin.get_data(df)
         self.assertEqual(x.shape, (13,))
         self.assertEqual(y.shape, (13,))
 
+    # test get_stats in data_lin module
     def test_get_stats(self):
         df = data_lin.pd.read_csv('../data/data_lin.csv')
         x, y = data_lin.get_data(df)
@@ -20,10 +23,11 @@ class TestDataLin(unittest.TestCase):
         self.assertEqual(p_value, 0.002646873922456106)
         self.assertEqual(std_err, 0.453536157607742)
 
-    #
+    # test function in data_lin module
     def test_function(self):
         self.assertEqual(data_lin.function(6, -1.7512877115526118, 103.10596026490066), 92.59823399558499)
 
+    # test plot in data_lin module
     def test_plot(self):
         df = data_lin.pd.read_csv('../data/data_lin.csv')
         x, y = data_lin.get_data(df)
@@ -31,5 +35,6 @@ class TestDataLin(unittest.TestCase):
         data_lin.plot(x, y, slope, intercept)
 
 
+# run the test
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
