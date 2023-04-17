ba7547c32b53b1f740cd775815873cf59956cfe6
Sasanka <sajewsasanka1@gmail.com>
2023-04-17 10:06:50 +0530

    Update test_data_lin.py
    - Change the data set location

--- supervisedml/unit_test/test_data_lin.py
+++ supervisedml/unit_test/test_data_lin.py
@@ -5,24 +5,24 @@ import Advertising
 class TestAdvertising(unittest.TestCase):
 
     def test_get_data(self):
-        df = Advertising.pd.read_csv('data/Advertising.csv')
+        df = Advertising.pd.read_csv('../data/Advertising.csv')
         x, y = Advertising.get_data(df)
         self.assertEqual(x.shape, (200, 3))
         self.assertEqual(y.shape, (200,))
 
     def test_create_model(self):
-        df = Advertising.pd.read_csv('data/Advertising.csv')
+        df = Advertising.pd.read_csv('../data/Advertising.csv')
         model = Advertising.create_model(df)
         self.assertEqual(model.coef_.shape, (3,))
         self.assertEqual(model.intercept_.shape, ())
         self.assertEqual(model.predict([[2300, 130, 0]]).shape, (1,))
 
     def test_plot_reg(self):
-        df = Advertising.pd.read_csv('data/Advertising.csv')
+        df = Advertising.pd.read_csv('../data/Advertising.csv')
         Advertising.test_plot_reg(df)
-
+    
     def test_plot_bar(self):
-        df = Advertising.pd.read_csv('data/Advertising.csv')
+        df = Advertising.pd.read_csv('../data/Advertising.csv')
         Advertising.test_plot_bar(df)
 
 
